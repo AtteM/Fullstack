eba070342b7fce63548b809c66fba7dcb8b21f87
Atte Möttönen <atte.mottonen@gmail.com>
2023-05-18 04:56:55 +0300

    2.19*: maiden tiedot, step2

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <div>
       <Filter filter={newFilter} handleFilterChange={handleFilterChange}/>
-      <Countries countries={countries} filter={newFilter}/>
+      <Countries countries={countries} filter={newFilter} setNewFilter={setNewFilter}/>
     </div>
   )
 
--- osa2/maiden_tiedot/src/components/Countries.js
+++ osa2/maiden_tiedot/src/components/Countries.js
@@ -1,23 +1,31 @@
 import Country from './Country'
 
-const Countries = ({countries, filter}) => {
+const Countries = ({countries, filter, setNewFilter}) => {
+
   if (!filter) {
     return
   }
   const filteredCountries = countries.filter(country =>
     country.name.common.toLowerCase().includes(filter.toLowerCase()))
 
-  if (filteredCountries.length === 1) {
+  const exactMatch = countries.filter(country => country.name.common === filter)
+  
+  if (exactMatch.length === 1) {
+    return (
+        <div>
+            <Country country={exactMatch[0]}/>
+        </div>
+    )
+  } else if (filteredCountries.length === 1) {
     return (
         <div>
             <Country country={filteredCountries[0]}/>
         </div>
     )
-
   } else if (filteredCountries.length <= 10) {
     return (
       <div>
-        {filteredCountries.map(country => <p key={country.cca3}>{country.name.common}</p>)}
+        {filteredCountries.map(country => <p key={country.cca3}>{country.name.common} <button onClick={() => setNewFilter(country.name.common)}>show</button></p>)}
       </div>
     )
   } else {
