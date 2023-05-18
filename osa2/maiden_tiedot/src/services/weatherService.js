dd39abc3bf17c5cfd53615be365bb9762721ff7e
Atte Möttönen <atte.mottonen@gmail.com>
2023-05-18 07:54:59 +0300

    2.20*: maiden tiedot, step3 updated

---
+++ osa2/maiden_tiedot/src/services/weatherService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+const api_key = process.env.REACT_APP_API_KEY
+
+const getLatLon = (cityName, countryCode) => {
+  const request = axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${api_key}`)
+  return request.then(response => response.data)
+}
+
+const getCurrentWeather = (lat,lon) => {
+  const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`)
+  return request.then(response => response.data)
+}
+
+export default {
+    getLatLon,
+    getCurrentWeather
+}
