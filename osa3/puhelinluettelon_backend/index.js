f2a1f8aed92cc5d842afa8e41ed0e330a0505a08
Atte Möttönen <atte.mottonen@gmail.com>
2023-05-26 05:27:52 +0300

    3.16: puhelinluettelo ja tietokanta, step4

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -25,34 +25,41 @@ app.get('/info', (req, res) => {
   //res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}<p>`)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person)
-  })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-// TODO
-app.delete('/api/persons/:id', (req, res) => {
-  /*
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
-  */
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!req.body.name) {
-    return res.status(400).json({
-      error: 'name missing'
-    })
+    const error = new Error('name missing')
+    error.name = 'EmptyNameError'
+    next(error)
+    return
   }
 
   if (!req.body.number) {
-    return res.status(400).json({
-      error: 'number missing'
-    })
+    const error = new Error('number missing')
+    error.name = "EmptyNumberError"
+    next(error)
+    return
   }
 
   const person = new Person({
@@ -64,6 +71,30 @@ app.post('/api/persons', (req, res) => {
     res.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'EmptyNameError') {
+    return res.status(400).send({ error: error.message })
+  } else if (error.name === 'EmptyNumberError') {
+    return res.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
